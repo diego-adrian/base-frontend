13d20c2d33eeb228a153dee5a8b6f033b80009e1
Adrian <dbarra@agetic.gob.bo>
2020-01-09 19:10:07 -0400

    cambios

--- src/plugins/notifications/AppNotifications.vue
+++ src/plugins/notifications/AppNotifications.vue
@@ -1,57 +1,102 @@
 <template>
-  <div>
-    <v-bottom-sheet
+  <v-dialog
       v-model="show"
-      inset
-      content-class="bottom--sheet"
+      width="500"
+      persistent
+      offset-y
+      offset-x
+      content-class="app--progress"
       transition="scale-transition"
-      persistent>
-      <v-sheet
-        scrollable
-        class="text-center"
-        :height="'50vh'"
-      >
-        <v-list>
-          <template v-for="(notificacion, index) in notificaciones">
-            <v-subheader
-              v-if="notificacion.header"
-              :key="notificacion.header"
-            >
-              {{ notificacion.header }}
-            </v-subheader>
+    >
+      <v-card>
+        <v-container fluid>
+          <v-row justify="center">
+            <v-subheader>Today</v-subheader>
 
-            <v-divider
-              v-else-if="notificacion.divider"
-              :key="index"
-              :inset="notificacion.inset"
-            ></v-divider>
+            <v-expansion-panels inset>
+              <v-expansion-panel
+                v-for="(message, i) in messages"
+                :key="i"
+                hide-actions
+              >
+                <v-expansion-panel-header>
+                  <v-row
+                    align="center"
+                    class="spacer"
+                    no-gutters
+                  >
+                    <v-col
+                      cols="4"
+                      sm="2"
+                      md="1"
+                    >
+                      <v-avatar
+                        size="36px"
+                      >
+                        <img
+                          v-if="message.avatar"
+                          alt="Avatar"
+                          src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+                        >
+                        <v-icon
+                          v-else
+                          :color="message.color"
+                          v-text="message.icon"
+                        ></v-icon>
+                      </v-avatar>
+                    </v-col>
 
-            <v-list-item
-              v-else
-              :key="notificacion.title"
-              @click=""
-            >
-              <v-list-item-avatar>
-                <v-img :src="notificacion.avatar"></v-img>
-              </v-list-item-avatar>
+                    <v-col
+                      class="hidden-xs-only"
+                      sm="5"
+                      md="3"
+                    >
+                      <strong v-html="message.name"></strong>
+                      <span
+                        v-if="message.total"
+                        class="grey--text"
+                      >
+                        &nbsp;({{ message.total }})
+                      </span>
+                    </v-col>
 
-              <v-list-item-content>
-                <v-list-item-title v-html="notificacion.title"></v-list-item-title>
-                <v-list-item-subtitle v-html="notificacion.subtitle"></v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-      </v-list>
+                    <v-col
+                      class="text-no-wrap"
+                      cols="5"
+                      sm="3"
+                    >
+                      <v-chip
+                        v-if="message.new"
+                        :color="`${message.color} lighten-4`"
+                        class="ml-0"
+                        label
+                        small
+                      >
+                        {{ message.new }} new
+                      </v-chip>
+                      <strong v-html="message.title"></strong>
+                    </v-col>
 
-        <v-btn
-          class="mt-6"
-          color="error"
-          outlined
-          @click="$store.commit('CLOSE_NOTIFICATIONS')"
-        >Cerrar notificaciones</v-btn>
-      </v-sheet>
-    </v-bottom-sheet>
-  </div>
+                    <v-col
+                      v-if="message.excerpt"
+                      class="grey--text text-truncate hidden-sm-and-down"
+                    >
+                      &mdash;
+                      {{ message.excerpt }}
+                    </v-col>
+                  </v-row>
+                </v-expansion-panel-header>
+
+                <v-expansion-panel-content>
+                  <v-divider></v-divider>
+                  <v-card-text v-text="lorem"></v-card-text>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-dialog>
 </template>
 <script>
 import { mapState } from 'vuex';
@@ -59,7 +104,32 @@ import { mapState } from 'vuex';
 export default {
   data: () => ({
     dialog: false,
-    messages: true,
+    messages: [
+      {
+        avatar: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
+        name: 'John Leider',
+        title: 'Welcome to Vuetify.js!',
+        excerpt: 'Thank you for joining our community...',
+      },
+      {
+        color: 'red',
+        icon: 'people',
+        name: 'Social',
+        new: 1,
+        total: 3,
+        title: 'Twitter',
+      },
+      {
+        color: 'teal',
+        icon: 'local_offer',
+        name: 'Promos',
+        new: 2,
+        total: 4,
+        title: 'Shop your way',
+        exceprt: 'New deals available, Join Today',
+      },
+    ],
+    lorem: 'Lorem ipsum dolor sit amet, at aliquam vivendum vel, everti delicatissimi cu eos. Dico iuvaret debitis mel an, et cum zril menandri. Eum in consul legimus accusam. Ea dico abhorreant duo, quo illum minimum incorrupte no, nostro voluptaria sea eu. Suas eligendi ius at, at nemore equidem est. Sed in error hendrerit, in consul constituam cum.',
     notificaciones: [
       { header: 'Notificaciones' },
       {
@@ -93,4 +163,9 @@ export default {
 };
 </script>
 <style lang="scss">
+  .app--progress {
+    position: fixed;
+    top: 0;
+    right: 0;
+  }
 </style>
